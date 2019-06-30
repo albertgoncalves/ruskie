6728d6445238d18b51ea92d6fb2b0f193ca8cb9b
albertgoncalves <goncalves.albert@gmail.com>
2019-06-30 12:17:08 -0400

    everything non_snake_case...

--- dev/src/test_schedules.rs
+++ dev/src/test_schedules.rs
@@ -1,9 +1,9 @@
+#[allow(non_snake_case)]
 #[cfg(test)]
 mod test {
     use serde::Deserialize;
     use serde_json::Number;
 
-    #[allow(non_snake_case)]
     #[derive(Deserialize, Debug, PartialEq)]
     struct Game {
         gamePk: Number,
@@ -13,7 +13,6 @@ mod test {
     const INPUT: &str = r#"{"gamePk": 20180304170}"#;
 
     // https://github.com/serde-rs/json/issues/340
-    #[allow(non_snake_case)]
     #[test]
     fn gamePk_number() {
         let json: Game = serde_json::from_str(INPUT).unwrap();
@@ -25,7 +24,6 @@ mod test {
         )
     }
 
-    #[allow(non_snake_case)]
     #[test]
     fn gamePk_string() {
         let json: Game = serde_json::from_str(INPUT).unwrap();
