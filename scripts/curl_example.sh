a7b99ed8436e42af5e77519644e6dca30d11a3f3
albertgoncalves <goncalves.albert@gmail.com>
2019-06-30 14:40:32 -0400

    adjust curl example shell script

--- scripts/curl_example.sh
+++ scripts/curl_example.sh
@@ -6,17 +6,23 @@ echo $WD/data
 echo $START
 echo $END
 
-curl "https://statsapi.web.nhl.com/api/v1/teams" \
-    > "$WD/data/teams-$START-$END.json"
+f () {
+    if [ ! -f $2 ]; then
+        curl $1 > $2
+    fi
+}
+
+f "https://statsapi.web.nhl.com/api/v1/teams" \
+    "$WD/data/teams/$START-$END.json"
 
 team_id="54"
 
-curl "https://statsapi.web.nhl.com/api/v1/schedule?teamId=$team_id&startDate=$START&endDate=$END" \
-    > "$WD/data/schedule-$team_id-$START-$END.json"
+f "https://statsapi.web.nhl.com/api/v1/schedule?teamId=$team_id&startDate=$START&endDate=$END" \
+    "$WD/data/schedule/$team_id-$START-$END.json"
 
 game_id="2018020861"
 
-curl "http://www.nhl.com/stats/rest/shiftcharts?cayenneExp=gameId=$game_id" \
-    > "$WD/data/shifts-$game_id.json"
-curl "https://statsapi.web.nhl.com/api/v1/game/$game_id/feed/live?site=en_nhl" \
-    > "$WD/data/game-$game_id.json"
+f "http://www.nhl.com/stats/rest/shiftcharts?cayenneExp=gameId=$game_id" \
+    "$WD/data/shifts/$game_id.json"
+f "https://statsapi.web.nhl.com/api/v1/game/$game_id/feed/live?site=en_nhl" \
+    "$WD/data/game/$game_id.json"
