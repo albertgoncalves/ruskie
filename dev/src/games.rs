134f6742ce2465f2f08e95580f056c11ef196fa7
albertgoncalves <goncalves.albert@gmail.com>
2019-07-01 21:58:02 -0400

    insert player event

--- dev/src/games.rs
+++ dev/src/games.rs
@@ -1,22 +1,21 @@
 mod blobs;
 mod sql;
 mod theft;
-mod vars;
 mod void;
 
 use crate::blobs::read_json;
 use crate::sql::connect;
 use crate::theft::{filename, get_to_file};
-use crate::vars::gather;
 use crate::void::ResultExt;
 use rusqlite::Connection;
 use serde::Deserialize;
 use serde_json::Number;
 use std::collections::HashMap;
+use std::env::var;
 use std::path::PathBuf;
 
 #[allow(non_snake_case)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Result {
     event: String,
     secondaryType: Option<String>,
@@ -24,67 +23,68 @@ struct Result {
     penaltyMinutes: Option<u8>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Coordinates {
     x: Option<Number>,
     y: Option<Number>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Goals {
     away: u8,
     home: u8,
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct About {
-    eventIdx: Number,
-    eventId: Number,
+    eventId: u16,
     period: u8,
+    periodType: String,
     periodTime: String,
     periodTimeRemaining: String,
     goals: Goals,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct PlayerId {
     id: Number,
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Participant {
     player: PlayerId,
     playerType: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct TeamId {
     id: u16,
 }
 
-#[derive(Deserialize)]
+#[allow(non_snake_case)]
+#[derive(Deserialize, Clone)]
 struct Person {
     id: Number,
     fullName: String,
-    shootsCatches: String,
+    shootsCatches: Option<String>,
     rosterStatus: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Position {
     r#type: String,
     abbreviation: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Player {
     person: Person,
     position: Position,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Event {
     result: Result,
     about: About,
@@ -94,36 +94,36 @@ struct Event {
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Plays {
     allPlays: Vec<Event>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Team {
     team: TeamId,
     players: HashMap<String, Player>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Teams {
     home: Team,
     away: Team,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Boxscore {
     teams: Teams,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct LiveData {
     plays: Plays,
     boxscore: Boxscore,
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct Events {
     gamePk: Number,
     liveData: LiveData,
@@ -132,11 +132,21 @@ struct Events {
 #[derive(Deserialize)]
 struct Shifts {}
 
+const QUERY_GAME_IDS: &str = {
+    "SELECT id \
+     FROM games \
+     WHERE status_abstract = 'Final' \
+     AND status_detailed = 'Final' \
+     AND type = 'R' \
+     ORDER BY DATE(date) ASC \
+     LIMIT 20;"
+};
+
 const CREATE_PLAYERS: &str = {
     "CREATE TABLE IF NOT EXISTS players \
      ( game_id TEXT \
+     , team_id INTEGER NOT NULL \
      , id TEXT NOT NULL \
-     , team_id INTEGER \
      , full_name TEXT NOT NULL \
      , shoots_catches TEXT \
      , roster_status TEXT NOT NULL \
@@ -150,8 +160,8 @@ const CREATE_PLAYERS: &str = {
 const INSERT_PLAYERS: &str = {
     "INSERT INTO players \
      ( game_id \
-     , id \
      , team_id \
+     , id \
      , full_name \
      , shoots_catches \
      , roster_status \
@@ -160,11 +170,53 @@ const INSERT_PLAYERS: &str = {
      ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8);"
 };
 
-const QUERY_GAME_IDS: &str = {
-    "SELECT id \
-     FROM games \
-     WHERE status_abstract = 'Final' \
-     AND status_detailed = 'Final';"
+const CREATE_EVENTS: &str = {
+    "CREATE TABLE IF NOT EXISTS events \
+     ( game_id TEXT \
+     , team_id TEXT NOT NULL \
+     , player_id TEXT \
+     , player_type TEXT NOT NULL \
+     , id INTEGER NOT NULL \
+     , event TEXT NOT NULL \
+     , secondary_type TEXT \
+     , penality_severity TEXT \
+     , penality_minutes INTEGER \
+     , period INTEGER NOT NULL \
+     , period_type TEXT NOT NULL \
+     , period_time TEXT NOT NULL \
+     , period_time_remaining TEXT NOT NULL \
+     , away_score INTEGER NOT NULL \
+     , home_score INTEGER NOT NULL \
+     , x REAL \
+     , y REAL \
+     , FOREIGN KEY (player_id, game_id) REFERENCES players(id, game_id) \
+     , UNIQUE(game_id, player_id, id) \
+     ); "
+};
+
+const INSERT_EVENTS: &str = {
+    "INSERT INTO events
+     ( game_id \
+     , team_id \
+     , player_id \
+     , player_type \
+     , id \
+     , event \
+     , secondary_type \
+     , penality_severity \
+     , penality_minutes \
+     , period \
+     , period_type \
+     , period_time \
+     , period_time_remaining \
+     , away_score \
+     , home_score \
+     , x \
+     , y \
+     ) VALUES \
+     ( ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16
+     , ?17
+     );"
 };
 
 #[inline]
@@ -203,16 +255,14 @@ fn scrape_pair<'a>(
     })
 }
 
-fn insert_events(t: &Connection, events: Events) {
-    println!("{}", events.gamePk.to_string());
-    for (_, player) in events.liveData.boxscore.teams.home.players.into_iter()
-    {
+fn insert_player(t: &Connection, game_id: &str, team: Team) {
+    for (_, player) in team.players.into_iter() {
         t.execute(
             INSERT_PLAYERS,
             &[
-                &events.gamePk.to_string(),
+                &game_id,
+                &team.team.id,
                 &player.person.id.to_string(),
-                &events.liveData.boxscore.teams.home.team.id.to_string(),
                 &player.person.fullName,
                 &player.person.shootsCatches,
                 &player.person.rosterStatus,
@@ -220,7 +270,50 @@ fn insert_events(t: &Connection, events: Events) {
                 &player.position.abbreviation,
             ],
         )
-        .void();
+        .void()
+    }
+}
+
+fn insert_players(t: &Connection, game_id: &str, away: Team, home: Team) {
+    insert_player(t, game_id, away);
+    insert_player(t, game_id, home);
+}
+
+fn insert_events(t: &Connection, game_id: &str, events: Events) {
+    for play in events.liveData.plays.allPlays {
+        if let Some(players) = play.players {
+            for player in players {
+                t.execute(
+                    INSERT_EVENTS,
+                    &[
+                        &game_id,
+                        &play.team.clone().map(|t| t.id),
+                        &player.player.id.to_string(),
+                        &player.playerType,
+                        &play.about.eventId,
+                        &play.result.event,
+                        &play.result.secondaryType,
+                        &play.result.penaltySeverity,
+                        &play.result.penaltyMinutes,
+                        &play.about.period,
+                        &play.about.periodType,
+                        &play.about.periodTime,
+                        &play.about.periodTimeRemaining,
+                        &play.about.goals.away,
+                        &play.about.goals.home,
+                        &play
+                            .coordinates
+                            .clone()
+                            .and_then(|c| c.x.and_then(|x| x.as_f64())),
+                        &play
+                            .coordinates
+                            .clone()
+                            .and_then(|c| c.y.and_then(|y| y.as_f64())),
+                    ],
+                )
+                .void()
+            }
+        }
     }
 }
 
@@ -228,9 +321,10 @@ fn insert_events(t: &Connection, events: Events) {
 // }
 
 fn main() {
-    if let Some((_, _, wd)) = gather() {
+    if let Ok(wd) = var("WD") {
         if let Ok(mut c) = connect(&wd) {
             c.execute(CREATE_PLAYERS, &[]).void();
+            c.execute(CREATE_EVENTS, &[]).void();
             if let Ok(ids) = c.prepare(QUERY_GAME_IDS).and_then(|mut s| {
                 s.query_map(&[], |r| {
                     let id: String = r.get("id");
@@ -253,7 +347,15 @@ fn main() {
                                 // (events, shifts)
                                 (events, Some(()))
                             } {
-                                insert_events(&t, events);
+                                let teams: Teams =
+                                    events.liveData.boxscore.teams.clone();
+                                let away: Team = teams.away.clone();
+                                let home: Team = teams.home;
+                                let game_id: String =
+                                    events.gamePk.to_string();
+                                println!("{}", &game_id);
+                                insert_players(&t, &game_id, away, home);
+                                insert_events(&t, &game_id, events);
                                 // insert_shifts(&t, shifts)
                             }
                         }
