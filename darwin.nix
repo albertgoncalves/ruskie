72a2faf64fa26a3ab55d4ac6a3a5ada3c50dc093
albertgoncalves <goncalves.albert@gmail.com>
2019-07-03 22:23:39 -0400

    python viz stack

--- darwin.nix
+++ darwin.nix
@@ -2,6 +2,11 @@
 with pkgs; mkShell {
     name = "Rust";
     buildInputs = [
+        (python37.withPackages(ps: with ps; [
+            flake8
+            matplotlib
+            pandas
+        ]))
         gtk2
         jq
         rlwrap
