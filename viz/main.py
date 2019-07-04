5b9582485e67765acdfdfa982d980fd8b83e8207
albertgoncalves <goncalves.albert@gmail.com>
2019-07-03 22:53:50 -0400

    implement rink viz

--- viz/main.py
+++ viz/main.py
@@ -2,23 +2,50 @@
 
 from os import environ
 
+from matplotlib import patches
 from matplotlib.pyplot import close, savefig, subplots, tight_layout
 from pandas import read_csv
 
 
 class Plot:
     def setup(data):
-        fig, ax = subplots(figsize=(12, 6))
-        ax.scatter(data.x, data.y, s=15, alpha=0.2)
-        return fig, ax
+        _, ax = subplots(figsize=(6.5, 5.5))
+        data.sort_values("goal", inplace=True)
+        ax.scatter(
+            data.x,
+            data.y,
+            c=data.goal.map({0: "c", 1: "tomato"}),
+            s=25,
+            alpha=0.75,
+        )
+        return ax
 
     def background(ax):
-        for f in [ax.axvline, ax.axhline]:
-            f(0, c="k", lw=0.5)
+        kwargs = {"alpha": 0.25, "zorder": 0}
+        ax.axvline(0, c="r", lw=7, **kwargs)
+        ax.axvline(29, c="b", lw=7, **kwargs)
+        ax.axhline(0, c="k", lw=1.5, **kwargs)
+        for xy in [(69, 22), (69, -22)]:
+            ax.add_patch(patches.Circle(
+                xy,
+                15,
+                lw=2,
+                fill=None,
+                **kwargs,
+            ))
+        ax.add_patch(patches.Rectangle(
+            (87, -3),
+            2,
+            6,
+            color="k",
+            **kwargs,
+        ))
 
     def aspect(ax):
-        ax.set_xlim([-100, 100])
+        ax.set_xlim([-15, 100])
         ax.set_ylim([-45, 45])
+        ax.set_xticks([])
+        ax.set_yticks([])
         ax.set_aspect("equal")
         tight_layout()
 
@@ -30,7 +57,7 @@ class Plot:
 def main():
     # $ cd $WD
     # $ cat sql/shots.sql | sql ruskie.db -csv > viz/data.csv
-    _, ax = Plot.setup(read_csv("{}/viz/data.csv".format(environ["WD"])))
+    ax = Plot.setup(read_csv("{}/viz/data.csv".format(environ["WD"])))
     Plot.background(ax)
     Plot.aspect(ax)
     Plot.export(ax, "{}/viz/shots.png".format(environ["WD"]))
