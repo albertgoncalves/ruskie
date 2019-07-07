e6e28ade4d267c3a1475336153afdcd113d5dd43
albertgoncalves <goncalves.albert@gmail.com>
2019-07-07 16:34:01 -0400

    simple error handling

--- model/scripts/convert.py
+++ model/scripts/convert.py
@@ -5,13 +5,18 @@ from sys import argv
 
 def transform(csv, libsvm):
     for x in csv:
-        xs = x.split(",")
-        libsvm.write(str(int(xs[0])))
-        for i in range(len(xs) - 1):
-            y = float(xs[i + 1])
-            if y != 0.0:
-                libsvm.write(" {}:{}".format(i, y))
-        libsvm.write("\n")
+        try:
+            xs = x.split(",")
+            libsvm.write(str(int(xs[0])))
+            for i in range(len(xs) - 1):
+                y = float(xs[i + 1])
+                if y != 0.0:
+                    libsvm.write(" {}:{}".format(i, y))
+            libsvm.write("\n")
+        except Exception as e:
+            print(e)
+            print(argv[1], x)
+            exit(1)
 
 
 def main():
