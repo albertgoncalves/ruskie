b0ee2fd92741dec0e7efc8e5dee59997296db21f
albertgoncalves <goncalves.albert@gmail.com>
2019-07-07 16:34:27 -0400

    plot goal mouth aperture

--- model/scripts/plot.py
+++ model/scripts/plot.py
@@ -11,7 +11,7 @@ from viz import export, rink
 def data():
     X = read_csv(
         "{}/model/data/gen.csv".format(environ["WD"]),
-        names=["_", "x", "y", "l", "r"],
+        names=["_", "x", "y", "l", "r", "a"],
     )
     Y = read_csv("{}/model/out/pred.csv".format(environ["WD"]), names=["z"])
     X["z"] = Y.z
@@ -23,10 +23,25 @@ def data():
 
 
 def plot(lr):
-    _, axs = subplots(2, 1, figsize=(5, 10))
+    _, axs = subplots(3, 1, figsize=(4, 12))
+    axs[0].tricontourf(
+        lr["left"].x,
+        lr["left"].y,
+        lr["left"].a,
+        cmap="PuBuGn",
+        alpha=0.75,
+    )
+    axs[0].set_title("aperture")
     for (i, k) in enumerate(lr.keys()):
-        axs[i].tricontourf(lr[k].x, lr[k].y, lr[k].z, cmap="Oranges", alpha=0.5)
-        axs[i].set_title(k)
+        axs[i + 1].tricontourf(
+            lr[k].x,
+            lr[k].y,
+            lr[k].z,
+            cmap="Oranges",
+            alpha=0.5,
+        )
+        axs[i + 1].set_title(k)
+    for i in range(3):
         rink(axs[i], zorder=3)
     export("{}/model/out/plot.png".format(environ["WD"]))
 
