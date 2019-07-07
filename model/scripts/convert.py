0296e0b42a73e6d5fcf9d0890113074482216713
albertgoncalves <goncalves.albert@gmail.com>
2019-07-07 14:00:03 -0400

    remove unused flag from convert.py

--- model/scripts/convert.py
+++ model/scripts/convert.py
@@ -3,28 +3,21 @@
 from sys import argv
 
 
-def transform(csv, libsvm, flag):
+def transform(csv, libsvm):
     for x in csv:
         xs = x.split(",")
-        if flag:
-            libsvm.write(str(int(xs[0])))
-            for i in range(len(xs) - 1):
-                y = float(xs[i + 1])
-                if y != 0.0:
-                    libsvm.write(" {}:{}".format(i, y))
-        else:
-            for i in range(len(xs)):
-                y = float(xs[i])
-                if y != 0.0:
-                    libsvm.write(" {}:{}".format(i, y))
+        libsvm.write(str(int(xs[0])))
+        for i in range(len(xs) - 1):
+            y = float(xs[i + 1])
+            if y != 0.0:
+                libsvm.write(" {}:{}".format(i, y))
         libsvm.write("\n")
 
 
 def main():
-    flag = bool(int(argv[3]))
     with open(argv[2], "w") as libsvm:
         with open(argv[1], "r") as csv:
-            transform(csv, libsvm, flag)
+            transform(csv, libsvm)
 
 
 if __name__ == "__main__":
